8bbe8f26929044ae18a860398f733ba0b2bed6b8
ellen-woodward <98536796+ellen-woodward@users.noreply.github.com>
2022-12-12 10:25:35 +0000

    Add files via upload

---
+++ Week11/PastExam/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace PastExam
+{
+    abstract class Account
+    {
+        #region properties
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime InterestDate { get; set; }
+
+        #endregion properties
+
+        #region constructors
+
+        public Account(string firstName, string lastName, decimal balance, DateTime interestDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Balance = balance;
+            InterestDate = interestDate;
+        }
+
+        public Account() { }
+
+        public Account(string firstName, string lastName) :this(firstName, lastName, 0, DateTime.Now) { }
+
+        #endregion constructors
+
+        public decimal Deposit(decimal amount)
+        {
+            return Balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            return Balance -= amount;
+        }
+
+        public abstract int CalculateInterest();
+    }
+}
+
